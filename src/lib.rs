8878a1309bcd056f0c87be5d35b3120dc272c5d2
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-06-09 20:55:40 +0800

    feat: determine codex home

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,12 @@
 #![deny(missing_docs)]
 #![warn(clippy::all, clippy::nursery, clippy::pedantic, clippy::cargo)]
 
-/// Add two numbers together.
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::{env::var, path::PathBuf};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Determine the path to Codex's home directory. ($CODEX_HOME or $HOME/.codex)
+pub fn codex_home() -> Option<PathBuf> {
+    var("CODEX_HOME")
+        .map(PathBuf::from)
+        .or_else(|_| var("HOME").map(|home| PathBuf::from(home).join(".codex")))
+        .ok()
 }
